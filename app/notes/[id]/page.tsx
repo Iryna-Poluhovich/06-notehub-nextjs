b806b1400e6625d73aa57cfb53da06ad25904034
POLIUKHOVYCH IRYNA <iryna0425@gmail.com>
2025-08-24 19:24:06 +0300

    upgrade-finish

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,11 +2,14 @@ import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-interface NoteDetailsPageProps {
-  params: { id: string };
-}
+// interface NoteDetailsPageProps {
+//   params: { id: string };
+// }
 
-export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+export default async function NoteDetailsPage({ params,
+}: {
+  params: { id: string };
+}) {
   const queryClient = new QueryClient();
   const noteId = Number(params.id);
 
